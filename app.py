bd14b70f637b7dbd0674765d9a246398a19c7191
Ayush Walia <ahluwailayush20@gmail.com>
2026-03-17 19:13:28 +0530

    DB created

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from model import db
 
-app = Flask(__name__)
+app = Flask(__name__)#creating flask app 
 
 # ================= CONFIG =================
+#We use app.config to store application settings like database URI,
+#  security key, and SQLAlchemy options so that Flask extensions can use them. 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hotel.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "123456"
 
 # ================= DB INIT =================
-db = SQLAlchemy(app)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 # ================= ROUTES =================
--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model):#here we are creating a table called User
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
